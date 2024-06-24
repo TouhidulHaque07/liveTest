1388ca61f1af215b5a20412896906cb59283032e
TouhidulHaque07 <munna054037@gmail.com>
2024-06-24 23:28:33 +0600

    ok

--- bin/Live_test_01.dart
+++ bin/Live_test_01.dart
@@ -1,4 +1,33 @@
+
+class Media{
+  late String name;
+
+  Media(String artistName){
+    this.name=artistName;
+  }
+
+   void play(){
+     print('Playing media. $name');
+   }
+}
+
+class song extends Media{
+  song(String artistName) : super(artistName);
+  
+  @override
+  void play() {
+    print('Playing song by $name');
+
+  }
+
+}
+
+
 void main(){
 
-  print('Github test');
+  Media Alomgir=Media('Alomgir');
+  Alomgir.play();
+  song Runa=song('Runa Lila');
+  Runa.play();
+
 }
